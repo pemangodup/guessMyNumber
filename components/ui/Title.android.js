f2a298c0340d3a9dde0ae94e8015a4a267f7ae45
Pema Ngodup <pemangodup@Pemas-MBP.Home>
2025-09-17 15:14:37 +0100

    Adding platform specific by adding .android or .ios extention

---
+++ components/ui/Title.android.js
@@ -0,0 +1,24 @@
+import { Text, StyleSheet, Platform } from "react-native";
+
+function Title({ children }) {
+  return <Text style={styles.text}>{children}</Text>;
+}
+export default Title;
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "open-sans-bold",
+    fontSize: 24,
+    // fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+    borderColor: "white",
+    // borderWidth: Platform.OS === "android" ? 0 : 2,
+    // borderWidth: Platform.select({ ios: 0, android: 0.5 }),
+    borderWidth: 0.5,
+    borderRadius: 5,
+    padding: 12,
+    maxWidth: "80%",
+    width: 300,
+  },
+});
